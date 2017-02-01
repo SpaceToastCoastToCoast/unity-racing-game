c9c678a22062617a259860d23142802f110eafe3
Alan <SpaceToastCoastToCoast@users.noreply.github.com>
2017-02-01 12:26:12 -1000

    add example script

---
+++ lesson-2/FinishLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour {
+	int laps = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player") || other.CompareTag ("Enemy")) {
+			laps++;
+			Debug.Log (other.name + " is now driving lap " + laps + ".");
+		}
+	}
+}
