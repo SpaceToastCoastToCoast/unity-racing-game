b6b6beeef6a74790fc9801c797b0300b5e2352cb
Alan <SpaceToastCoastToCoast@users.noreply.github.com>
2017-02-02 14:40:32 -1000

    add example files

---
+++ lesson-5/FinishLine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour {
+	Hashtable positions;
+	public static Dictionary<Collider, int> playerLaps;
+	// Use this for initialization
+	void Start () {
+		positions = new Hashtable ();
+		playerLaps = new Dictionary<Collider, int> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player") || other.CompareTag ("Enemy")) {
+			//We want to save the position at which this object came in, and compare it to its position in the next frame.
+			if (!positions.ContainsKey(other)) {
+				positions.Add (other, other.transform.position);
+				Debug.Log (other.name + " has entered the finish line.");
+			}
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (positions.ContainsKey(other)) {
+			Vector3 prevPos = (Vector3)positions[other];
+			if (prevPos.z - other.transform.position.z < 0f) {
+				if (!playerLaps.ContainsKey (other)) {
+					playerLaps.Add (other, 0);
+				} else {
+					playerLaps [other] = (int)playerLaps [other] + 1;
+				}
+				Debug.Log (other.name + " has completed " + playerLaps [other] + " laps.");
+			} else {
+				//Take away when they cross in a negative direction
+				playerLaps [other] = (int)playerLaps [other] - 1;
+			}
+			positions.Remove (other);
+		}
+	}
+}
---
+++ lesson-5/WinGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour {
+
+	Text text;
+	float timeToNext = 5f;
+	float timer = 0f;
+	string winner;
+
+	// Use this for initialization
+	void Awake () {
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FinishLine.playerLaps.ContainsValue (3) && !text.enabled) {
+
+			foreach(KeyValuePair<Collider, int> p in FinishLine.playerLaps) {
+				if (p.Value == 3) {
+					winner = p.Key.name;
+					break;
+				}
+			}
+
+			text.enabled = true;
+
+			text.text = (winner + " has won!");
+		}
+		if (text.enabled) {
+			timer += Time.deltaTime;
+			if (timer >= timeToNext) {
+				timer = 0f;
+				SceneManager.LoadScene (0);
+			}
+		}
+	}
+}
